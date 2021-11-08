e2031861d24527d153ff1ae0992f3e2ea464e064
Noah Friedman <noahfriedman2@gmail.com>
2021-11-08 10:38:11 -0500

    Version 0.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,4 @@
 # CHANGELOG
+
+### [v0.1.0](https://github.com/noah-friedman/kmap/tree/v0.1.0)
+- Initial release
--- bin/kmap/main.go
+++ bin/kmap/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/noah-friedman/kmap"
+	"os"
+)
 
+func main() {
+	fmt.Println("kmap v0.1.0")
+	fmt.Println("Created by Noah Friedman")
+	fmt.Println()
+
+	c, e := kmap.Program(os.Stdin, os.Stdout)
+
+	if e != nil {
+		fmt.Printf("ERROR: %s", e.Error())
+	}
+
+	os.Exit(c)
 }
